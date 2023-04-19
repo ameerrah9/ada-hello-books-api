8b78d005d5c8dc56bcb932eefba07e245fa3b414
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-04-18 22:17:34 -0400

    Connect DB + Setup Model

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask
+# import SQLAlchemy from flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+# import Migrate from flask_migrate
+from flask_migrate import Migrate
+
+# give us access to database operations
+# instantiate the db
+db = SQLAlchemy()
+# instantiate the migrate
+migrate = Migrate()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # set up the database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    # connect the db and migrate to our Flask app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     # register the books blueprint
     from .routes import books_bp
     app.register_blueprint(books_bp)
-    
+
+    from app.models.book import Book
+
     return app
